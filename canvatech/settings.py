2056254c04fb3fa5e3a330697168d08d2e51e59b
AnujMishra <anujm77000@gmail.com>
2026-01-30 20:17:20 +0530

    email_brevo

--- canvatech/settings.py
+++ canvatech/settings.py
@@ -147,7 +147,7 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 EMAIL_HOST = os.getenv("EMAIL_HOST")
-EMAIL_PORT = int(os.getenv("EMAIL_PORT"))
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") == "True"
 
 EMAIL_HOST_USER = os.getenv("BREVO_SMTP_LOGIN")
