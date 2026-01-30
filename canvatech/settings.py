663f1ebfa74ebfda427f6169e73259dd7dea2e5c
AnujMishra <anujm77000@gmail.com>
2026-01-30 17:34:06 +0530

    cloudinary

--- canvatech/settings.py
+++ canvatech/settings.py
@@ -45,7 +45,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'backend',
+    'cloudinary',
+    'cloudinary_storage',
+   
 ]
+
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -121,7 +127,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -154,6 +159,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "backend/static", 
 ]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 
@@ -164,3 +172,10 @@ DATABASES = {
         ssl_require=True,
     )
 }
+
+
+
+
+CLOUDINARY_CLOUD_NAME= os.getenv("CLOUDINARY_CLOUD_NAME"),
+CLOUDINARY_API_KEY= os.getenv("CLOUDINARY_API_KEY"),
+CLOUDINARY_API_SECRET= os.getenv("CLOUDINARY_API_SECRET"),
